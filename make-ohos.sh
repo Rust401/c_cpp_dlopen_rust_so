d911c2916047bd1b295c1325aadff9ef9faac68e
rust <31315527+Rust401@users.noreply.github.com>
2025-01-21 19:15:29 +0800

    Update make-ohos.sh

--- make-ohos.sh
+++ make-ohos.sh
@@ -3,22 +3,14 @@ cargo clean
 
 # build .a
 # cargo build --release
-export RUSTFLAGS="-Z has_thread_local=true -Z tls-model=local-dynamic"
+# export RUSTFLAGS="-Z has_thread_local=true -Z tls-model=local-dynamic"
 
 CC=$(pwd)/oh_sdk/aarch64-unknown-linux-ohos-clang.sh
 CXX=$(pwd)/oh_sdk/aarch64-unknown-linux-ohos-clang++.sh
 
-#export RUSTFLAGS="-C target-feature=-emutls"
-#export RUSTFLAGS="-C llvm-args=-disable-emulated-tls"
-#export RUSTFLAGS="-C llvm-args=-fno-emulated-tls"
-#export RUSTFLAGS="-C link-args=-fno-emulated-tls"
-#export RUSTFLAGS="-C link-args=-Wl,-plugin-opt=-emulated-tls=0"
-
 export RUSTFLAGS="-C linker=${CC}"
 
 CC=$CC CXX=$CXX cargo build --verbose --release --target aarch64-unknown-linux-ohos
-
-#cp ./target/aarch64-unknown-linux-ohos/cxxbridge/rust_to_cpp/src/lib.rs.cc rs.cpp
 
 cp ./target/aarch64-unknown-linux-ohos/release/libdylib_hello.so .
 
